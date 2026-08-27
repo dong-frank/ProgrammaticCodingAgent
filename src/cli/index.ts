0ae620069f0825fa3267b1acf31f01f2a0a32455
dong <1057762929@qq.com>
2026-08-27 20:10:13 +0800

    chore: 接入 dotenv 自动加载 .env 配置

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+import "dotenv/config";
 import { Command } from "commander";
 import { isMode, MODES } from "../modes/types.ts";
 import { createLlmClientFromEnv } from "../llm/client.ts";
